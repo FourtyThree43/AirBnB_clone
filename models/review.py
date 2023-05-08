7ea42838d83131ae20c4261e7c51374a0788bdcb
joyce-source <brownjojo97@gmail.com>
2023-05-08 12:34:49 -0700

    review&state

---
+++ models/review.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines class review"""
+
+
+class Review(BaseModule):
+    """Public class attributes that inherit from BaseModel"""
+    place_id: str =''
+    user_id: str =''
+    text: str =''
---
+++ models/state.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defines class state"""
+
+
+class State(BaseModel):
+    """Public class attributes that inherit from BaseModel"""
+    name: str = ''
