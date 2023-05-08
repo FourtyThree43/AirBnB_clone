d60f49576dec5b3b48f891f822039e53362dd0a1
joyce-source <brownjojo97@gmail.com>
2023-05-08 12:33:58 -0700

    amenity&place

---
+++ models/amenity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class amenity"""
+
+
+class amenity(BaseModel):
+    """Public class attributes that inherit from BaseModel"""
+    name: str =''
---
+++ models/place.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" defines a class place"""
+
+
+class Place(BaseModel):
+"""Public class attributes that inherit from BaseModel"""
+    city_id: str = ''
+    user_id: str = ''
+    name: str = ''
+    description: str = ''
+    number_rooms: int = 0
+    number_bathrooms: int = 0
+    max_guest: int = 0
+    price_by_night: int = 0
+    latitude: float = 0.0
+    longitude: float = 0.0
+    amenity_ids: List[str] = []
