87077751b68dcc0a40fc899c0272d77b528964e3
FourtyThree43 <shaqmwa@outlook.com>
2023-05-16 20:40:00 +0300

    FS

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 
 import unittest
 import os
+import json
 from models.base_model import BaseModel
 from models import storage
 from models.engine.file_storage import FileStorage
@@ -61,15 +62,30 @@ class TestFileStorage(unittest.TestCase):
 
     def test_reload(self):
         """Test reload method"""
-        storage = FileStorage()
-        my_model = BaseModel()
-        key = "{}.{}".format(type(my_model).__name__, my_model.id)
-        storage.new(my_model)
-        storage.save()
-        # self.assertEqual(len(storage.all()), 2)
+
+        test_object = {
+            "BaseModel.1234-1234-1234": {
+                "id": "1234-1234-1234",
+                "created_at": "2023-05-16T15:16:19.011124",
+                "updated_at": "2023-05-16T15:16:19.011126",
+                "__class__": "BaseModel"
+            }
+        }
+
+        temp_file_path = "temp_file.json"
+        with open(temp_file_path, mode='w', encoding='utf-8') as file:
+            json.dump(test_object, file)
+
+        storage._FileStorage__file_path = temp_file_path
+        storage._FileStorage__objects = test_object
         storage.reload()
-        # self.assertEqual(len(storage.all()), 2)
-        self.assertIn(key, storage.all().keys())
+        self.assertEqual(len(storage._FileStorage__objects), 1)
+        self.assertIsInstance(storage._FileStorage__objects["BaseModel.1234-1234-1234"], BaseModel)
+        self.assertEqual(storage._FileStorage__objects["BaseModel.1234-1234-1234"].id, "1234-1234-1234")
+        # self.assertEqual(storage._FileStorage__objects["BaseModel.1234-1234-1234"].created_at, "2023-05-16T15:16:19.011124")
+        # self.assertEqual(storage._FileStorage__objects["BaseModel.1234-1234-1234"].updated_at, "2023-05-16T15:16:19.011126")
+
+        os.remove(temp_file_path)
 
 
 if __name__ == '__main__':
